2eb33d7e17b230bde1ac33140e29c4dfe754ff2d
santosfk <santosguilhermefk@gmail.com>
2022-04-25 19:13:49 -0300

    feat: criando rotas

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
-
+const { randomUUID } = require("crypto");
 const app = express();
-app.get("/rota", (request, response) => {
-  return response.json({ message: "alo garaio" });
+const products = [];
+
+app.post("/produtos", (request, response) => {
+  const body = request.body;
+  const product = {
+    name: "papel",
+    price: "200",
+    id: randomUUID(),
+  };
+  products.push(product);
+  response.json(product);
+  return console.log(body);
+});
+app.get("/produtos", (request, response) => {
+  return response.json(products);
 });
 app.listen(3002, () => console.log("servidor rodando na porta 3002"));
