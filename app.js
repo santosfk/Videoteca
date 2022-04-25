2769f0b8f46144a5d43ebb5cc79d6ff4f5bb631f
santosfk <santosguilhermefk@gmail.com>
2022-04-25 19:48:25 -0300

    feat: adicionando mais produtos

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ const products = [];
 
 app.post("/produtos", (request, response) => {
   const body = request.body;
-  const product = {
-    name: "papel",
-    price: "200",
-    id: randomUUID(),
-  };
+  const product = [
+    {
+      name: "papel",
+      price: "200",
+      id: randomUUID(),
+    },
+    {
+      name: "borracha",
+      price: "300",
+      id: randomUUID(),
+    },
+  ];
   products.push(product);
   response.json(product);
   return console.log(body);
 });
 app.get("/produtos", (request, response) => {
   return response.json(products);
+});
+app.get("/produtos/:id", (request, response) => {
+  const { id } = request.params;
+  const product = products.find((product) => product.id === id);
+  return response.json(product);
 });
 app.listen(3002, () => console.log("servidor rodando na porta 3002"));
