a037cc1f1d09024f8a7bfb81e9dcfab8140ea30e
santosfk <santosguilhermefk@gmail.com>
2022-04-28 10:25:17 -0300

    feat : criando arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const routes = express.Router();
-
+const videoController = require("./controllers/videoController");
 routes.get("/", (req, res) => {
   res.send("olá companheiro");
 });
+routes.get("/videos", videoController.index);
+routes.post("/videos", videoController.store);
 module.exports = routes;
