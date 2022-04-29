680958327b87591c55f3315b79b691c4bdf24497
santosfk <santosguilhermefk@gmail.com>
2022-04-29 20:04:56 -0300

    feat: criando o componente do botao que adiciona novos videos

--- client/front/src/App.tsx
+++ client/front/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
+import AddButton from "./components/AddButton/AddButton";
 function App() {
   return (
     <div>
---
+++ client/front/src/components/AddButton/AddButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AddButton() {
+  return (
+    <div className="w-full flex justify-center">
+      <button className="bg-slate-800 text-white p-4 rounded-md w-48 hover:bg-slate-900 ">
+        Adicionar videos
+      </button>
+    </div>
+  );
+}
