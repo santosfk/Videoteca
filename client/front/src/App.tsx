e954afeb3dd034635fac319fa53df2ae4b8a0fb4
santosfk <santosguilhermefk@gmail.com>
2022-04-29 19:49:22 -0300

    feat: criando o componente de header

--- client/front/src/App.tsx
+++ client/front/src/App.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-
-import "./App.css";
-
+import Header from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <h2>alo krai</h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          aprendendo react
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Footer />;
     </div>
   );
 }
