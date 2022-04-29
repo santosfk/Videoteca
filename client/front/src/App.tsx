ae03bae6d5f2daca19707b637694720c1d37aa97
santosfk <santosguilhermefk@gmail.com>
2022-04-29 11:50:31 -0300

    feat : criando estrutura de pastas onde a pasta client recebe o front da aplicação e a pasta de service recebe o back

---
+++ client/front/src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h2>alo krai</h2>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          aprendendo react
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
