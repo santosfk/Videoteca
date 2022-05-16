9eed3596ebf6bcd55e7d04ec36b9bda34c90fe2f
santosfk <santosguilhermefk@gmail.com>
2022-05-15 22:08:37 -0300

    feat/creating favorites modal

---
+++ client/front/src/components/FavoritesModal/FavoriteModal.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FavoriteModal() {
+  return (
+    <div className="bg-white">
+      <h1>favoritos</h1>
+      <h2>descrição dos favoritos</h2>
+    </div>
+  );
+}
--- client/front/src/components/Header/Header.tsx
+++ client/front/src/components/Header/Header.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsGithub } from "react-icons/bs";
 import { AiFillHeart as Heart } from "react-icons/ai";
+import FavoriteModal from "../FavoritesModal/FavoriteModal";
 export default function Header() {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <div className="w-full p-4 bg-slate-800 inline-flex justify-between  items-center ">
+    <div className="w-full px-11 py-5 bg-slate-800 inline-flex justify-between items-center ">
+      {openModal && <FavoriteModal />}
       <h1 className="text-left text-2xl text-white">videoteca</h1>
-      <div className="flex">
+      <div className="flex gap-9">
+        <Heart
+          className="text-3xl text-slate-100 cursor-pointer"
+          onClick={() => setOpenModal(true)}
+        />
         <a href="https://github.com/santosfk/Videoteca">
           <BsGithub className="text-3xl text-slate-100" />
         </a>
-        <Heart className="text-3xl text-slate-100" />
       </div>
     </div>
   );
