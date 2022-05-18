a5482ca6a4f1b358fd3276773d08550db9f218d2
santosfk <santosguilhermefk@gmail.com>
2022-05-17 21:56:58 -0300

    feat/aligning favorites modal items

--- client/front/src/components/FavoritesModal/FavoriteModal.tsx
+++ client/front/src/components/FavoritesModal/FavoriteModal.tsx
@@ -4,19 +4,23 @@ import { Videos } from "../../types/videos";
 export default function FavoriteModal() {
   const { data } = useAxios("videos");
   return (
-    <div className="bg-white absolute w-72 h-48 p-4 z-10 top-16 right-5 rounded-lg shadow-xl flex items-center flex-col">
+    <div className="bg-white absolute w-96 h-48 p-4 z-10 top-16 right-5 rounded-lg shadow-xl flex items-center flex-col justify-center">
       {data?.videos?.map((video: Videos) => {
         if (video.like.valueOf() === true) {
           return (
-            <div className="flex inline-flex gap-3 items-center m-2">
-              <h1 className="text-lg text-center  w-2/4">{video.title}</h1>
-              <a
-                href={video.link}
-                target="_blank"
-                className=" bg-slate-600 p-1 w-12 text-center text-white rounded-md"
-              >
-                ver
-              </a>
+            <div className="flex inline-flex justify-center items-center m-2 w-full ">
+              <div className=" w-2/4 flex items-center justify-center">
+                <h1 className="text-lg text-center">{video.title}</h1>
+              </div>
+              <div className=" w-2/4 flex items-center">
+                <a
+                  href={video.link}
+                  target="_blank"
+                  className=" bg-slate-600 p-1 w-12 text-center text-white rounded-md"
+                >
+                  ver
+                </a>
+              </div>
             </div>
           );
         }
